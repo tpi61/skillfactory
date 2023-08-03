86e43ea2171c30dbd6984c0b2ad2f75a0d287cd1
tpi <pirogovta@gmail.com>
2023-08-04 02:04:50 +0300

    added sea battle

--- sea battle/sea_battle.py
+++ sea battle/sea_battle.py
@@ -1,59 +1,127 @@
-from pprint import pprint
+import random
 
-class Ship:
-    def __init__(self, size, msg):
+class Board:
+    def __init__(self, size=6) -> None: 
         self.__size = size
-        self.__coordinates = []
-        counter = 0
-        while counter != self.__size:
-            coordinates = input(msg)
-            coordinates = coordinates.split(' ')
-            for coordinate in coordinates:
-                if self.__size != counter:
-                    try:
-                        coordinate = tuple(map(int, [number for number in coordinate]))
-                    except:
-                        print('Only digit can be used')
-                        continue
-                    
-                    if len(coordinate) != 2:
-                        print('2 digits need to be set')
-                    elif not all(0 < number < 7 for number in coordinate):
-                        print('digits must be in range 1-6')
-                    else:
-                        self.__coordinates.append(coordinate)
-                        counter+=1
+        self.__fields: dict[tuple, int] = {}
+        for y in range(1,self.__size+1):
+            for x in range(1,self.__size+1):
+                self.__fields.setdefault((y,x), 0)
+        self.__ships: list = []
+        self.__unshot = list(self.__fields.keys())
 
     @property
-    def size(self):
+    def get_size(self) -> int:
         return self.__size
     
     @property
-    def coordinates(self):
-        return self.__coordinates
+    def fields(self) -> dict:
+        return self.__fields
     
-    @coordinates.setter
-    def coordinates(self, coordinate):
-        self.__coordinates = coordinate
+    @fields.setter
+    def fields(self,change_field) -> None:
+        for key,value in change_field:
+            self.__fields[key] = value
+    
+    @property
+    def ships(self) -> list:
+        return self.__ships
+    
+    @ships.setter
+    def ships(self, ship) -> None:
+        for pos in ship.coordinates:
+            self.__ships.append(pos)
+            self.__fields[pos] = 1
 
-class Board:
-    def __init__(self):
-        self.__coordinates = {}
-        for y in range(1,7):
-            for x in range(1,7):
-                self.__coordinates.setdefault((y,x), "O") 
-        
-    @property    
-    def coordinates(self):
+    def rm_ship(self, shot) -> None:
+        self.__ships.remove(shot)
+    
+    @property
+    def unshot(self) -> list:
+        return self.__unshot
+    
+    @unshot.setter
+    def unshot(self, shot) -> None:
+        self.__unshot.remove(shot)
+
+class EnamyBoard(Board):
+    def __init__(self,size=6) -> None:
+        super().__init__(size)
+        self.__fields_mist = self.fields.copy()
+
+    @property
+    def fields_mist(self) -> dict[tuple, int]:
+        return self.__fields_mist
+    
+    @fields_mist.setter
+    def fields_mist(self, change_field) -> None:
+        for key,value in change_field:
+            self.__fields_mist[key] = value
+
+class Ship:
+    def __init__(self,size,coordinates) -> None:
+        self.__size = size
+        self.__coordinates = coordinates
+
+    @property
+    def size(self) -> int:
+        return self.__size
+    
+    @property
+    def coordinates(self) -> list:
         return self.__coordinates
+
+class Shot:
+    def shot_check(self, shot, target_board) -> bool:
+        if target_board.fields[shot] in [2,3]:
+            return False
+        else:
+            return True
+
+    def random_shot(self, unshot) -> tuple:
+        return random.choice(unshot)
     
-    @coordinates.setter
-    def coordinates(self, ship):
-        #ship = tuple([int(index) for index in ship])
-        self.__coordinates
-        self.__coordinates[ship] = "■"
-    
-   
+    def __init__(self, target_board, hyman=True) -> None:
+        self.win = False
+        while True:
+            try:
+                shot = None
+                if hyman:
+                    print('Please make a shot: ', end=' ')
+                    shot = tuple(map(int,list(input())))
+                else:
+                    shot = self.random_shot(target_board.unshot)
+                if len(shot) == 2:
+                    if self.shot_check(shot, target_board):
+                        if not hyman: print(f'Enamy shot {shot}\n')
+                        if shot in target_board.ships:
+                            target_board.rm_ship(shot)
+                            target_board.fields[shot] = 2
+                            if hyman: target_board.fields_mist[shot] = 2
+                            target_board.unshot = shot
+                            if hyman:
+                                print('Hit!\n')
+                            else:
+                                print('Enamy Hit\n')
+                            if not target_board.ships:
+                                self.win = True
+                                break
+                            continue
+                        else:
+                            target_board.fields[shot] = 3
+                            if hyman: target_board.fields_mist[shot] = 3
+                            target_board.unshot = shot
+                            break
+                    else:
+                        if hyman: print('Try to do anather shot')
+                        continue
+                else:
+                    if hyman: print('Please input two digit (example: 23): ')
+                    continue
+            except:
+                if hyman: print('\nPlease input only digits!\n')
+                continue
+
 class BattleField:
     def __init__(self, player_board,enemy_board):
         self.__player_board = player_board
@@ -62,85 +130,137 @@ class BattleField:
     def display_bf(self):
         def index():
             for n in range(1,7):
-                    yield n
+                yield n
+
+        def digit_to_sign(value):
+            if value == 0:
+                    return "O"
+            elif value == 1:
+                    return "■"
+            elif value == 2:
+                    return "X"
+            elif value == 3:
+                    return "."
 
         board = f'\tENEMY BOARD\t\t\tPLAYER BOARD\n'    
         board += f'  | 1 | 2 | 3 | 4 | 5 | 6 |\t  | 1 | 2 | 3 | 4 | 5 | 6 |\n'
         for y in index():
             board += f"{y} "
             for x in index():
-                board += f'| {self.__enemy_board.coordinates[y,x]} '
+                board += f'| {digit_to_sign(self.__enemy_board.fields_mist[y,x])} '
             board += "|\t"
             board += f"{y} "
             for x in index():
-                board += f'| {self.__player_board.coordinates[y,x]} '
+                board += f'| {digit_to_sign(self.__player_board.fields[y,x])} '
             board += "|\n"
-        return board
+        return board    
 
-class Main:
-    def __init__(self):
-        self.player_board = Board()
-        self.enemy_board = Board()
-        self.mask_board = Board()
-        self.bf = BattleField(self.player_board,self.mask_board)
+class Game:
+    def __init__(self) -> None:
+        self.p_board = Board()
+        self.e_board = EnamyBoard()
+        self.bf = BattleField(self.p_board,self.e_board)
+    
+    def refresh(self) -> None:
+        print(self.bf.display_bf())
 
+    def ship_check(self, coordinates, any_board, hyman):
 
-    def initiate(self):
-        print(self.bf.display_bf(),'\n')
-        print('Please set ships')
+        def is_range(check_list):
+            srt = sorted(check_list)
+            return srt == list(range(srt[0], srt[0] + len(srt)))
 
-        def ship_check(coordinates):
-            for coordinate in coordinates:
-                if self.player_board.coordinates[coordinate] == 'O':
-
-                    if self.player_board.coordinates.get((coordinate[0]-1,coordinate[1])) != "■" \
-                        and self.player_board.coordinates.get((coordinate[0]+1,coordinate[1])) != "■" \
-                        and self.player_board.coordinates.get((coordinate[0],coordinate[1]-1)) != "■" \
-                        and self.player_board.coordinates.get((coordinate[0],coordinate[1]+1)) != "■" :
+            ### CHECK ON BUSY
+        if all([True if any_board.fields[coordinate] == 0 else False for coordinate in coordinates]):
+            ### CHECK ON NEAR BUSY
+            if all([True if any_board.fields.get((coordinate[0]-1,coordinate[1])) != 1 else False for coordinate in coordinates])\
+                and all([True if any_board.fields.get((coordinate[0]+1,coordinate[1])) != 1 else False for coordinate in coordinates])\
+                and all([True if any_board.fields.get((coordinate[0],coordinate[1]-1)) != 1 else False for coordinate in coordinates])\
+                and all([True if any_board.fields.get((coordinate[0],coordinate[1]+1)) != 1 else False for coordinate in coordinates]):
+                ### HORIZONTAL ONE LINE CHECK
+                if all(coordinate[0] == coordinates[0][0] for coordinate in coordinates):
+                    if is_range([coordinate[1] for coordinate in coordinates]):
                         return True
                     else:
-                        print('Ships are to close')
+                        if hyman: print('No white spase can be in ship')
+                        return
+                ### VERTICAL ONE LINE CHECK
+                elif all(coordinate[1] == coordinates[0][1] for coordinate in coordinates):
+                    if is_range([coordinate[0] for coordinate in coordinates]):
+                        return True
+                    else:
+                        if hyman: print('No white spase can be in ship')
                         return
                 else:
-                    print('Plase for the ship is busy')
+                    if hyman: print('Ship must be in one line')
                     return
+            else:
+                if hyman: print('Ships are to close')
+                return
+        else:
+            if hyman :print('Place for the ship is busy')
+            return
 
-        # while True:
-        #     treaple = Ship(3, 'Add coordinates of Treaple-ship ')            
+    def initiate(self, any_board, hyman=True) -> None:
+        greetings = {
+            'Add coordinates of a Treaple-ship (example: 23 24 25): ':3,
+            'Add coordinates of first Duble-ship (example: 23 24): ':2,
+            'Add coordinates of second Duble-ship (example: 23 24): ':2,
+            'Add coordinates of first Solo-ship (example: 23): ':1,
+            'Add coordinates of second Solo-ship (example: 23): ':1,
+            'Add coordinates of third Solo-ship (example: 23): ':1,
+        }
 
+        def rnd_coordinates(ship_size):
+            rand_coordinates = []
+            n = ship_size
+            while n:
+                n-=1
+                rand_coordinates.append((random.randint(1, 6),random.randint(1, 6)))
 
-        while True:
-            treaple = Ship(3, 'Add coordinates of Treaple-ship (example: 23 24 25): ')
-            if ship_check(treaple.coordinates):
-                print(treaple.coordinates)
-                self.player_board.coordinates = treaple.coordinates
-                break
-        print(self.bf.display_bf(),'\n')
-
-        while True:
-            uno1 = Ship(1, 'Add coordinates of first Solo-ship (example: 23): ')
-            if ship_check(uno1.coordinates):
-                self.player_board.coordinates = uno1.coordinates
-                break
-        print(self.bf.display_bf(),'\n')
-
-        while True:
-            uno2 = Ship(1, 'Add coordinates of second Solo-ship: ')
-            if ship_check(uno2.coordinates):
-                self.player_board.coordinates = uno2.coordinates
-                break
+            return rand_coordinates
         
-        print(self.bf.display_bf(),'\n')
+        if hyman: print('Please set ships\n')
+        for greeting,size in greetings.items():
 
+            while True:
+                if hyman: print(greeting, end='')
+                try:
+                    coordinates = []
+                    if hyman:
+                        coordinates = [tuple(map(int,list(n))) for n in input().split(' ')]
+                    else:
+                        coordinates = rnd_coordinates(size)
 
-start = Main()
-start.initiate()
+                    if len(coordinates) != size or not self.ship_check(coordinates, any_board, hyman):
+                        continue
+                    else:
+                        any_board.ships = Ship(size,coordinates)
+                        break
+                except:
+                    if hyman: print('\nPlease input only digits!\n')
+                    continue
 
-# #pl_b.board = '23'
-# pl_b.board[(2,4)] = '■'
-# #pl_b.board((1,1))
-# print(bf.display_bf())
-# print('Please set ships')
-# # a = input('Enter coordinates for Duble-ship: ')
-# # print(a)
-# #pprint(pl_b.board)
+    def fight(self):
+        while True:
+            self.refresh()
+            shot = Shot(self.e_board)
+            if shot.win:
+                print('CONGRATS! YOU ARE WIN')
+                self.refresh()
+                break
+            self.refresh()
+            shot = Shot(self.p_board, hyman=False)
+            if shot.win:
+                self.refresh()
+                print('YOU ARE LOOSE')
+                break
+
+    def run(self):
+        self.refresh()
+        self.initiate(self.p_board, hyman=False)
+        self.initiate(self.e_board, hyman=False)
+        self.fight()
+
+gm = Game()
+gm.run()
